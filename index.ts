eabb8fb038d1e767606735c26921d4119153dedc
Jake Robins <mendahu@gmail.com>
2020-11-16 13:29:17 -0800

    basic discord bot skeleton

---
+++ index.ts
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN_ID);
