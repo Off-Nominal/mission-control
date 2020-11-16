cf8042a07a4651d036de7d138b3d3cc29696946e
Jake Robins <mendahu@gmail.com>
2020-11-16 13:48:35 -0800

    dependencies install for dev environment

--- index.ts
+++ index.ts
@@ -7,4 +7,8 @@ client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
+client.on('message', (message) => {
+  if (message.content === `!bc`) console.log(message.content, 'yo');
+});
+
 client.login(process.env.DISCORD_TOKEN_ID);
