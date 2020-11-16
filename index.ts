cd638f15bbbff89c5d8ef430233b1b1d2fb0cb41
Jake Robins <mendahu@gmail.com>
2020-11-16 14:12:36 -0800

    implement message parsing for recommend command

--- index.ts
+++ index.ts
@@ -1,14 +1,36 @@
 require('dotenv').config();
 const Discord = require('discord.js');
+const axios = require('axios');
+const { prefix } = require('./config.json');
 
 const client = new Discord.Client();
+const BASEURL = process.env.BASEURL;
 
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on('message', (message) => {
-  if (message.content === `!bc`) console.log(message.content, 'yo');
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === `recommend`) {
+    switch (args[0]) {
+      case 'random':
+        console.log('random', args);
+        break;
+      case 'best':
+        console.log('highest rated', args);
+        break;
+      case 'favourite':
+        console.log('favourite', args);
+        break;
+      default:
+        console.log('some other command', args);
+    }
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN_ID);
