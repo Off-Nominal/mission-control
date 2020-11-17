fe72e159b4d472561558bcb7b98448580f8a3d9e
Jake Robins <mendahu@gmail.com>
2020-11-16 19:07:33 -0800

    refactor

--- index.js
+++ index.js
@@ -3,20 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require('dotenv').config();
 var Discord = require('discord.js');
 var prefix = require('./config.json').prefix;
-var axios = require('axios');
+var recommend_1 = require("./commands/recommend");
 var client = new Discord.Client();
-var BASEURL = process.env.BASEURL;
-var RecommendCommand;
-(function (RecommendCommand) {
-    RecommendCommand["random"] = "random";
-    RecommendCommand["highestrated"] = "highestrate";
-    RecommendCommand["favourite"] = "favourite";
-})(RecommendCommand || (RecommendCommand = {}));
-var ErrorType;
-(function (ErrorType) {
-    ErrorType["api"] = "api";
-    ErrorType["badCommand"] = "bad-command";
-})(ErrorType || (ErrorType = {}));
 client.once('ready', function () {
     console.log("Logged in as " + client.user.tag);
 });
@@ -26,43 +14,7 @@ client.on('message', function (message) {
     var args = message.content.slice(prefix.length).trim().split(/ +/);
     var command = args.shift().toLowerCase();
     if (command === "recommend") {
-        var sendError_1 = function (type, incorrectArg) {
-            var errorMessage;
-            switch (type) {
-                case 'api':
-                    errorMessage = 'Oh no, something seems to have gone wrong.';
-                    break;
-                case 'bad-command':
-                    errorMessage = "That recommend type (" + incorrectArg + ") is not supported";
-            }
-            message.channel.send(errorMessage);
-        };
-        var sendRecommendation_1 = function (slug) {
-            message.channel.send(BASEURL + "/books/" + slug);
-        };
-        var handleRecommendCommand = function (type) {
-            axios
-                .get(BASEURL + "/api/recommendations?type=" + type)
-                .then(function (response) { return sendRecommendation_1(response.data[0].slug); })
-                .catch(function (err) {
-                console.error(err);
-                sendError_1(ErrorType.api);
-            });
-        };
-        switch (args[0]) {
-            case 'random':
-                handleRecommendCommand(RecommendCommand.random);
-                break;
-            case 'best':
-                axios;
-                handleRecommendCommand(RecommendCommand.highestrated);
-                break;
-            case 'favourite':
-                handleRecommendCommand(RecommendCommand.favourite);
-                break;
-            default:
-                sendError_1(ErrorType.badCommand, args[0]);
-        }
+        recommend_1.handleRecommendCommand(args[0], message);
     }
 });
 client.login(process.env.DISCORD_TOKEN_ID);
--- index.ts
+++ index.ts
@@ -3,21 +3,9 @@ import { Message } from 'discord.js';
 require('dotenv').config();
 const Discord = require('discord.js');
 const { prefix } = require('./config.json');
-const axios = require('axios');
+import { handleRecommendCommand } from './commands/recommend';
 
 const client = new Discord.Client();
-const BASEURL = process.env.BASEURL;
-
-enum RecommendCommand {
-  random = 'random',
-  highestrated = 'highestrate',
-  favourite = 'favourite',
-}
-
-enum ErrorType {
-  api = 'api',
-  badCommand = 'bad-command',
-}
 
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
@@ -30,46 +18,7 @@ client.on('message', (message: Message) => {
   const command = args.shift().toLowerCase();
 
   if (command === `recommend`) {
-    const sendError = (type: ErrorType, incorrectArg?: string) => {
-      let errorMessage;
-      switch (type) {
-        case 'api':
-          errorMessage = 'Oh no, something seems to have gone wrong.';
-          break;
-        case 'bad-command':
-          errorMessage = `That recommend type (${incorrectArg}) is not supported`;
-      }
-      message.channel.send(errorMessage);
-    };
-
-    const sendRecommendation = (slug) => {
-      message.channel.send(`${BASEURL}/books/${slug}`);
-    };
-
-    const handleRecommendCommand = (type: RecommendCommand) => {
-      axios
-        .get(`${BASEURL}/api/recommendations?type=${type}`)
-        .then((response) => sendRecommendation(response.data[0].slug))
-        .catch((err) => {
-          console.error(err);
-          sendError(ErrorType.api);
-        });
-    };
-
-    switch (args[0]) {
-      case 'random':
-        handleRecommendCommand(RecommendCommand.random);
-        break;
-      case 'best':
-        axios;
-        handleRecommendCommand(RecommendCommand.highestrated);
-        break;
-      case 'favourite':
-        handleRecommendCommand(RecommendCommand.favourite);
-        break;
-      default:
-        sendError(ErrorType.badCommand, args[0]);
-    }
+    handleRecommendCommand(args[0], message);
   }
 });
 
