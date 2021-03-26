f43ab59c5df053d19c9dfb051d74b6e18847cd47
Jake Robins <me@jakerobins.com>
2021-03-26 16:22:40 -0700

    fix type error

--- src/utilityListeners/siteChecker/index.ts
+++ src/utilityListeners/siteChecker/index.ts
@@ -1,31 +1,51 @@
 import axios from "axios";
-import { MessageEmbed, TextChannel } from "discord.js";
+import { Client, MessageEmbed, TextChannel } from "discord.js";
 const Discord = require("discord.js");
 
-export const createSiteChecker = (client, url) => {
+export const createSiteChecker = (client: Client, url: string) => {
   let eTag = "";
 
   const checker = () => {
-    axios.head(url).then((res) => {
-      if (eTag === "") {
-        console.log("Setting eTag to ", res.headers.etag);
-        eTag = res.headers.etag;
-        return;
-      } else if (eTag !== res.headers.etag) {
-        console.log("eTag does not match, sending Discord message");
-        const embed: MessageEmbed = new Discord.MessageEmbed();
+    axios
+      .head(url)
+      .then((res) => {
+        let message = "eTag matches, carry on";
+        let messageNecessary = false;
 
-        embed
-          .setColor("#3e7493")
-          .setTitle(`FYI - there has been an update to the Starship website.`)
-          .setURL("https://www.spacex.com/vehicles/starship/");
+        if (res.status !== 200) {
+          message = `There was an error reaching the Starship site. Error code ${res.status}`;
+        } else if (eTag === "") {
+          eTag = res.headers.etag;
+          message = `Setting eTag to ${res.headers.etag}`;
+        } else if (eTag !== res.headers.etag) {
+          message = "eTag does not match, sending Discord message";
+          messageNecessary = true;
+        }
 
-        return client.channels
-          .fetch("781235493118672949")
-          .then((channel) => (channel as TextChannel).send(embed));
-      }
-      return console.log("eTag matches, carry on");
-    });
+        if (!messageNecessary) {
+          return message;
+        } else {
+          const embed: MessageEmbed = new Discord.MessageEmbed();
+          embed
+            .setColor("#3e7493")
+            .setTitle(`FYI - there has been an update to the Starship website.`)
+            .setURL("https://www.spacex.com/vehicles/starship/");
+
+          const messageSent = client.channels
+            .fetch("781235493118672949")
+            .then((channel) => (channel as TextChannel).send(embed))
+            .finally(() => {
+              const message = "eTag does not match, sending Discord message";
+              return message;
+            });
+        }
+      })
+      .then((message) => {
+        return console.log(message);
+      })
+      .catch((err) => {
+        return console.log(err);
+      });
   };
   return checker;
 };
