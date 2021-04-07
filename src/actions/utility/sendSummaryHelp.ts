e447d321d191b38aeca09d7a9f7263c650565995
Jake Robins <me@jakerobins.com>
2021-04-06 21:36:44 -0700

    help copy update

--- src/actions/utility/sendSummaryHelp.ts
+++ src/actions/utility/sendSummaryHelp.ts
@@ -4,7 +4,7 @@ export const sendSummaryHelp = (message: Message) => {
   const embed = new MessageEmbed();
 
   embed
-    .setTitle("Getting channel summaries")
+    .setTitle("Getting channel summaries [BETA]")
     .setDescription(
       "You can generate a summary of links posted to a channel by calling `!summary #` where `#` is a number of hours to go back, up to a maximum of 24.\n\nExample: `!summary 8` returns all the links in the last 8 hours.\n\nIf no number is specified, it will default to 8.\n\nDuplicates will be bundled together to reduce clutter."
     );
