045a78171d33d58582d25c9bd5333133e7cfec25
Jake Robins <me@jakerobins.com>
2021-06-28 11:13:24 -0700

    update regex for edge cases

--- src/actions/utility/translateTemp.ts
+++ src/actions/utility/translateTemp.ts
@@ -8,18 +8,35 @@ type Temperature = {
 };
 
 export const findTempsToConvert = (message: Message) => {
-  const regex = new RegExp(/(?<temp>-?\d{1,4}).{0,3}(?<unit>[FC])/gi);
+  const regex = new RegExp(
+    /(?<=^|\s)(?<sign>[-+]?)(?<temp1>\d+\.?\d*)-?(?<temp2>\d+\.?\d*)?°?\s?(?<unit>[FC])/gi
+  );
   const matches = message.content.matchAll(regex);
 
   const tempsToConvert: Temperature[] = [];
 
   for (const match of matches) {
+    const { sign, temp1, temp2, unit } = match.groups;
+
+    const value = sign ? -Number(temp1) : Number(temp1);
+
     const temp: Temperature = {
-      value: Number(match[1]),
-      unit: match[2] as Unit,
+      value,
+      unit: unit as Unit,
     };
 
     tempsToConvert.push(temp);
+
+    if (temp2) {
+      const value = Number(temp2);
+
+      const temp: Temperature = {
+        value,
+        unit: unit as Unit,
+      };
+
+      tempsToConvert.push(temp);
+    }
   }
 
   return tempsToConvert;
