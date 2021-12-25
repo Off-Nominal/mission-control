9b9c5634422bbbfad165ef1df96c3b65457e2561
Jake Robins <me@jakerobins.com>
2021-12-25 09:39:34 -0600

    update help command for bookclub

--- src/clients/bookclub/actions/generateHelpEmbed.ts
+++ src/clients/bookclub/actions/generateHelpEmbed.ts
@@ -7,22 +7,34 @@ export const generateHelpEmbed = () => {
   embed
     .setColor("#3e7493")
     .setTitle("Book Club Bot Help")
+    .setThumbnail(
+      "https://res.cloudinary.com/dj5enq03a/image/upload/v1640446534/Discord%20Assets/onbc_oauth_logo_wijdlq.png"
+    )
     .setDescription(
-      'Book Club Bot can serve you recommendations using the "recommend" command.'
+      "Book Club Bot can serve you recommendations for books in the Space Book Club application."
     )
     .addFields(
       {
-        name: "!bc recommend random",
+        name: "Using the bot",
+        value:
+          "Type `/bookclub` into the message bar and the bot will serve you different options for getting a recommendation.",
+      },
+      { name: "\u200B", value: "\u200B" },
+      {
+        name: "Random",
         value: "Gives you a random book from our collection.",
+        inline: true,
       },
       {
-        name: "!bc recommend best",
+        name: "Best",
         value:
           "Gives you the book with the highest user rating in the last 60 days.",
+        inline: true,
       },
       {
-        name: "!bc recommend favourite",
+        name: "Favourite",
         value: `Gives you the book with the most community favourites in the last 60 days. And yes, that's favourite with a "u". :flag_ca:`,
+        inline: true,
       }
     );
   return embed;
