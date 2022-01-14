f706dba19280da1a71815a83b708b628e6288a17
Jake Robins <me@jakerobins.com>
2022-01-14 11:30:12 -0600

    content bot ignores events that are not main content

--- src/clients/content/handlers/handleEventEnded.ts
+++ src/clients/content/handlers/handleEventEnded.ts
@@ -1,9 +1,14 @@
 import { GuildScheduledEvent } from "discord.js";
-import { FeedList } from "../../..";
+import { Feed, FeedList } from "../../..";
 
 export default function handleEventEnded(
   event: GuildScheduledEvent,
   feeds: FeedList
 ) {
-  //
+  const isYouTube = feeds[Feed.OFF_NOMINAL_YOUTUBE].getEpisodeByUrl(event.url);
+  const isHappyHour = feeds[Feed.HAPPY_HOUR].getEpisodeByUrl(event.url);
+
+  if (!isYouTube && !isHappyHour) {
+    return;
+  }
 }
