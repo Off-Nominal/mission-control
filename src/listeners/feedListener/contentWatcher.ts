815aa2cf7c9e5e8ec88a168ae3e2571949108d22
Jake Robins <me@jakerobins.com>
2022-07-01 13:16:57 -0500

    fix casing on robust watcher

--- src/listeners/feedListener/contentWatcher.ts
+++ src/listeners/feedListener/contentWatcher.ts
@@ -1,7 +1,7 @@
 import { GuildScheduledEvent } from "discord.js";
 import Fuse from "fuse.js";
 import { ContentFeedItem } from "../../clients/content/handlers/handleNewContent";
-import { RobustWatcher } from "./RobustWatcher";
+import { RobustWatcher } from "./robustWatcher";
 const FuseJS = require("fuse.js");
 
 const defaultProcessor = (item, showTitle: string) => item;
