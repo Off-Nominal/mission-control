cfeb3c70be011beeda3f290f4b72f6fa594cf17f
Jake Robins <me@jakerobins.com>
2023-01-10 21:01:17 -0600

    add catch for undefined promise

--- src/helpers/truncateText.ts
+++ src/helpers/truncateText.ts
@@ -1,5 +1,8 @@
-export const truncateText = (text: string, limit: number): string => {
-  if (text.length <= limit) {
+export const truncateText = (
+  text: string | undefined,
+  limit: number
+): string => {
+  if (!text || text.length <= limit) {
     return text;
   } else {
     return text.slice(0, limit - 3) + "...";
