536ef11f574e5dbb95e16ef7865a29c2d4795c7e
Jake Robins <me@jakerobins.com>
2023-09-10 09:02:44 -0600

    remove book club code

--- src/clients/main/actions/generateHelpEmbed.ts
+++ src/clients/main/actions/generateHelpEmbed.ts
@@ -8,7 +8,6 @@ export const generateHelpEmbed = () => {
     )
     .addFields([
       { name: "Content", value: "`/content help`", inline: true },
-      { name: "Book Club", value: "`/bookclub help`", inline: true },
       { name: "NostradamBot", value: "`!ndb help`", inline: true },
       { name: "Launch Alerts", value: "`!l help`", inline: true },
       { name: "Terminal Count", value: "`!tc help`", inline: true },
