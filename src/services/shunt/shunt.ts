d35ff493af7a661b572714eec0375264bdab7546
Jake Robins <me@jakerobins.com>
2025-05-06 11:07:01 -0600

    update discord js and refactor shunt with better logging

--- src/services/shunt/shunt.ts
+++ src/services/shunt/shunt.ts
@@ -4,22 +4,79 @@ import {
   EmbedBuilder,
   ChannelType,
   GuildBasedChannel,
+  InteractionCallbackResponse,
+  ChatInputCommandInteraction,
 } from "discord.js";
+import { Logger, LogInitiator, LogStatus } from "../../logger/Logger";
+
+// Embed Generator
+const generateEmbed = (options: {
+  direction: "inbound" | "outbound";
+  sourceChannelName: string;
+  targetChannelName: string;
+  shunterName: string;
+  topic: string;
+  url?: string;
+}) => {
+  const {
+    url,
+    direction,
+    sourceChannelName,
+    targetChannelName,
+    shunterName,
+    topic,
+  } = options;
+
+  const copy = {
+    inbound: {
+      title: `Incoming conversation from #${sourceChannelName}`,
+      description: `${shunterName}: "${topic}"${
+        url ? `- [Read the original](${url})` : ""
+      }`,
+      thumbnail: "https://i.imgur.com/kfvmby0.png",
+    },
+    outbound: {
+      title: `Conversation moving to #${targetChannelName}`,
+      description: `${shunterName}: "${topic}"${
+        url ? `- [Follow the conversation!](${url})` : ""
+      }`,
+      thumbnail: "https://i.imgur.com/UYBbaLR.png",
+    },
+  };
+
+  const { title, description, thumbnail } = copy[direction];
+
+  return new EmbedBuilder()
+    .setTitle(title)
+    .setDescription(description)
+    .setThumbnail(thumbnail);
+};
 
 export default async function shunt(
-  interaction: CommandInteraction,
+  interaction: ChatInputCommandInteraction,
   targetChannel: GuildBasedChannel,
   topic: string
 ) {
+  const logger = new Logger(
+    "Shunt Interaction",
+    LogInitiator.DISCORD,
+    "Shunt Command"
+  );
+
+  logger.addLog(
+    LogStatus.INFO,
+    `Received a Shunt command, validating data and initiating shunt.`
+  );
+
   const sourceChannel = interaction.channel;
 
   // Only accept shunts/threads from/to a text channel or thread
-  if (
-    (sourceChannel.type !== ChannelType.GuildText &&
-      sourceChannel.type !== ChannelType.GuildPublicThread) ||
-    (targetChannel.type !== ChannelType.GuildText &&
-      targetChannel.type !== ChannelType.GuildPublicThread)
-  ) {
+  if (!sourceChannel.isTextBased() || !targetChannel.isTextBased()) {
+    logger.addLog(
+      LogStatus.WARNING,
+      `Shunt command was not sent from non Text-based Channel.`
+    );
+    logger.sendLog(interaction.client);
     return interaction.reply({
       content:
         "The Shunt command only works from a Text Channel or a Public Thread. It won't work via DM or other sources.",
@@ -28,83 +85,131 @@ export default async function shunt(
 
   // Prevent shunting to same channel
   if (sourceChannel.id === targetChannel.id) {
+    logger.addLog(
+      LogStatus.INFO,
+      `Shunt command was targeting same channel, ignoring.`
+    );
+    logger.sendLog(interaction.client);
     return interaction.reply({
       content: "Cannot shunt to the same place.",
     });
   }
 
-  const shunter = interaction.member.user;
-  const shunterName = shunter.username;
+  const shunter = interaction.member;
 
-  // Embed Generator
-  const generateEmbed = (options: {
-    direction: "inbound" | "outbound";
-    url?: string;
-  }) => {
-    const { url, direction } = options;
-
-    const copy = {
-      inbound: {
-        title: `Incoming conversation from #${sourceChannel.name}`,
-        description: `${shunterName}: "${topic}"${
-          url ? `- [Read the original](${url})` : ""
-        }`,
-        thumbnail: "https://i.imgur.com/kfvmby0.png",
-      },
-      outbound: {
-        title: `Conversation moving to #${targetChannel.name}`,
-        description: `${shunterName}: "${topic}"${
-          url ? `- [Follow the conversation!](${url})` : ""
-        }`,
-        thumbnail: "https://i.imgur.com/UYBbaLR.png",
-      },
-    };
-
-    const { title, description, thumbnail } = copy[direction];
-
-    return new EmbedBuilder()
-      .setTitle(title)
-      .setDescription(description)
-      .setThumbnail(thumbnail);
-  };
+  // Fetch Server Nickname
+  let shunterName: string | null = null;
+  try {
+    shunterName = await interaction.guild.members
+      .fetch(shunter.user.id)
+      .then((member) => {
+        return member.nickname || member.user.username;
+      });
+  } catch (err) {
+    logger.addLog(
+      LogStatus.WARNING,
+      `There was an error fetching the shunter nickname. Falling back to username.`
+    );
+    console.error("Unable to fetch shunter nickname");
+    console.error(err);
+    shunterName = shunter.user.username;
+  }
 
   // Source Message
-  let sourceReply: Message<boolean> | null = null;
+  let sourceReply: InteractionCallbackResponse | null = null;
+
+  let success = true;
 
   try {
-    await interaction.reply({
+    sourceReply = await interaction.reply({
       embeds: [
         generateEmbed({
           direction: "outbound",
+          sourceChannelName: sourceChannel.name,
+          targetChannelName: targetChannel.name,
+          shunterName,
+          topic,
         }),
       ],
+      withResponse: true,
     });
-    sourceReply = await interaction.fetchReply();
+    logger.addLog(LogStatus.SUCCESS, `Shunt source message sent.`);
   } catch (err) {
+    success = false;
+    logger.addLog(
+      LogStatus.WARNING,
+      `There was an error sending the source message.`
+    );
     console.error("Unable to send Shunt/Thread Source Message");
     console.error(err);
   }
 
   // Destination Message
+  let destinationMessage: Message | null = null;
   try {
-    const destinationMessage = await targetChannel.send({
+    destinationMessage = await targetChannel.send({
       embeds: [
         generateEmbed({
-          url: sourceReply.url,
+          url: sourceReply.resource.message.url,
+          sourceChannelName: sourceChannel.name,
+          targetChannelName: targetChannel.name,
+          shunterName,
+          topic,
           direction: "inbound",
         }),
       ],
     });
-    return await interaction.editReply({
+
+    logger.addLog(LogStatus.SUCCESS, `Shunt destination message sent.`);
+  } catch (err) {
+    success = false;
+    logger.addLog(
+      LogStatus.WARNING,
+      `There was an error sending the destination message.`
+    );
+    console.error("Could send destintation embed");
+    console.error(err);
+  }
+
+  try {
+    await interaction.editReply({
       embeds: [
         generateEmbed({
           url: destinationMessage.url,
+          sourceChannelName: targetChannel.name,
+          targetChannelName: sourceChannel.name,
+          shunterName,
+          topic,
           direction: "outbound",
         }),
       ],
     });
+
+    logger.addLog(
+      LogStatus.SUCCESS,
+      `Shunt source message edited with destination URL.`
+    );
   } catch (err) {
-    console.error("Could send destintation embed or edit source message");
+    success = false;
+    logger.addLog(
+      LogStatus.WARNING,
+      `There was an error editing the source message with the destination URL.`
+    );
+    console.error("Unable to edit Shunt/Thread Source Message");
     console.error(err);
   }
+
+  if (success) {
+    logger.addLog(
+      LogStatus.SUCCESS,
+      `Shunt command was successfully executed.`
+    );
+  } else {
+    logger.addLog(
+      LogStatus.FAILURE,
+      `Shunt command was executed, but there were errors.`
+    );
+  }
+
+  return await logger.sendLog(interaction.client);
 }
